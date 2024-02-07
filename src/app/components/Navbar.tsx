fc51f7b5d6d955a1998a3abbd4899d4f1afd1eb1
LiliKaori <lilikaori10@gmail.com>
2024-02-07 12:16:41 -0300

    feat/login-clerk

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,10 +1,22 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 
 export default function Navbar (){
     return(
         <nav className="fixed top-0 w-full flex items-center py-2 px-8 justify-between z-50 bg-slate-800 text-gray-300">
           <Link href="/" className="uppercase font-bold text-md h-12 flex items-center">Next Store</Link>
-          <p>Menu</p>
+          <div className="flex items-center gap-8">
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
+            <SignedOut>
+                <SignInButton mode="modal">
+                    <button className="border border-gray-400 rounded-md px-3 py-2">
+                        Fazer Login
+                    </button>
+                </SignInButton>
+            </SignedOut>
+          </div>
         </nav>
     )
 }
